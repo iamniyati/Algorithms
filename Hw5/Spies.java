8820e7e0dc7c86fb10ae21ccf9fed92da6e2eef1
nxs6032 <nxs6032@g.rit.edu>
2016-10-17 19:55:08 -0400

    Update Spies.java

--- Hw5/Spies.java
+++ Hw5/Spies.java
@@ -16,7 +16,7 @@ import java.util.jar.Pack200;
  * @version   $Id$ 1.0 Spies.java
  *
  * @author   Niyati Shah(nxs6032) Section3
- *              & Rudresh Pandit(rmp7494) Section1
+ *              
  *
  * Revisions:
  *	$Log$
